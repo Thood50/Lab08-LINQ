f8b03b8dcb7979cf47caadbda6d4d0c47bff1227
Tyler Hood <tyler.r.hood@gmail.com>
2019-01-17 11:16:34 -0800

    Forgot to do enitial commit, got things set up got my json in. started to write it but reading docs I think i need my classes set up first, so imma find a reader online to give me what classes I need for this data

---
+++ LINQ/LINQ/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace LINQ
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static void ReadJSON()
+        {
+            string path = "../../../data.json";
+            string json = File.ReadAllText(path);
+            var data = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
+
+        }
+    }
+}
